c3725163a182aa924a35dd03674a34c3a88d3eab
mog13 <mog13@live.com>
2019-03-12 08:09:11 +0000

    finished umbrella example

---
+++ do-i-need-an-umbrella-or-not/main.sh
@@ -0,0 +1,13 @@
+source "./config"
+
+city=$(curl -L ifconfig.co/city | sed 's/ /%20/g')
+country=$(curl -L ifconfig.co/country-iso)
+precip=$(curl -s "https://api.weatherbit.io/v2.0/current?city=Milton%20Keynes&country=GB&key=d2cf09a6d3864dd0bf0a3c97dede2d37" | grep -E -o '.precip.{0,100}' | cut -d, -f 1 | cut -d: -f2)
+
+if [[ $precip < 0.5 ]]; then
+ echo "you need an umbrella today!"
+else
+ echo "leave that brolly at home!"
+fi
+
+
