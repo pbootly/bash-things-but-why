875947950f8074363f9b98a6ca31e40193288014
pbootly <mthomas@polybit.io>
2019-03-14 21:18:14 +0000

    delete swap files

--- do-i-need-an-umbrella-or-not/main.sh
+++ do-i-need-an-umbrella-or-not/main.sh
@@ -2,7 +2,7 @@ source "./config"
 
 city=$(curl -L ifconfig.co/city | sed 's/ /%20/g')
 country=$(curl -L ifconfig.co/country-iso)
-precip=$(curl -s "https://api.weatherbit.io/v2.0/current?city=${city}&country=${country}&key=d2cf09a6d3864dd0bf0a3c97dede2d37" | grep -E -o '.precip.{0,100}' | cut -d, -f 1 | cut -d: -f2)
+precip=$(curl -s "https://api.weatherbit.io/v2.0/current?city=${city}&country=${country}&key=${ApiKey}" | grep -E -o '.precip.{0,100}' | cut -d, -f 1 | cut -d: -f2)
 
 if [[ $precip < 0.5 ]]; then
  echo "you need an umbrella today!"
